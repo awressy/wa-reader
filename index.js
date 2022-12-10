52a7b1dc97042528ce5132e2600d2e48c34cf718
awressy <awressy@gmail.com>
2022-12-10 22:24:28 +0700

    read message in whatsapp

--- index.js
+++ index.js
@@ -4,23 +4,57 @@ const verification = async (page) => {
     try {
         await page.goto("https://web.whatsapp.com", { waitUntil: 'domcontentloaded' });
         await page.waitForSelector('._2UwZ_');
+        delay(5000)
 	    await page.waitForSelector("div#side._1KDb8", { waitUntil: 'domcontentloaded' });
-        await console.log("koka");
+        // await console.log("koka");
     } catch (err) {
         console.log(err);
+        // continue;
     }
 }
 
+const checkGreen = async (page) => {
+    while (true) {
+        try {
+            delay(1000)
+            await page.waitForXPath("//span[contains(@aria-label,'unread')]/ ancestor::div[@class='_3OvU8']", { waitUntil: 'load', timeout: 0 })
+            const elHandle = await page.$x("//span[contains(@aria-label,'unread')]/ ancestor::div[@class='_3OvU8']");
+			await elHandle[0].click();
+        } catch (err) {
+            console.log(err);
+            delay(10000)
+            continue;
+        }
+    }
+}
+
+const sendAutoMessage = async (page) => {
+
+}
+
 async function main(){
-    const browser = await puppeteer.launch({headless: false});
-	const page = await browser.newPage();
-	await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36');
-	await page.setRequestInterception(true);
-    await page.on('request', (request) => {
-		if (request.resourceType() === 'image') request.abort();
-		else request.continue();
-	});
-    verification(page)
+    try {
+        const browser = await puppeteer.launch({headless: false});
+        const page = await browser.newPage();
+        await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36');
+        await page.setRequestInterception(true);
+        await page.on('request', (request) => {
+            if (request.resourceType() === 'image') request.abort();
+            else request.continue();
+        });
+        verification(page)
+        delay(5000)
+        checkGreen(page)
+    } catch (err) {
+        console.log(err);
+    }
+    
 }
 
 main()
+
+function delay(time) {
+	return new Promise(function (resolve) {
+		setTimeout(resolve, time);
+	});
+}
